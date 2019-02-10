dce48598d3733cb6dc1b896f38b814dbb0cb7679
Addison-Stavlo <AddieStavlo@gmail.com>
2019-02-10 17:48:14 -0500

    first page and nav bar set up, need CTA btn on first page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 const triangle = document.querySelector(".slide-angle");
 const body = document.querySelector("body");
+const navBar = document.querySelector(".nav-bar");
+const slideAngle = document.querySelector(".slide-angle");
+const myName = document.querySelector(".my-name");
+const myTrade = document.querySelector(".my-trade");
 // console.log(triangle);
 // triangle.style.borderLeft = `${window.innerWidth * 0.7}px solid transparent`;
 // triangle.style.borderBottom = `${window.innerHeight}px solid #555`;
@@ -13,9 +17,24 @@ function setSlideWidth() {
     triangle.style.borderLeft = `${body.clientWidth}px solid transparent`;
   }
 }
-window.onload = () => {
+
+document.onreadystatechange = () => {
   setSlideWidth();
   triangle.style.borderBottom = `${window.innerHeight}px solid #555`;
+  TweenMax.from(navBar, 1.2, {
+    y: -5000,
+    ease: Sine.easeOut,
+    onComplete: () => {
+      //   navBar.removeAttribute("style");
+    }
+  });
+  TweenMax.from(slideAngle, 0.6, { right: -1000, ease: Sine.easeOut });
+  TweenMax.from(myName, 2, { opacity: 0 });
+  TweenMax.from(myTrade, 2, { opacity: 0 });
+};
+window.onload = () => {
+  //   triangle.style.borderBottom = `${window.innerHeight}px solid #555`;
+  // TweenMax.from(navBar, 2, { height: 0, opacity: 0 });
 };
 
 window.onresize = () => {

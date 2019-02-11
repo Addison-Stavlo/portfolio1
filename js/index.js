9c5b5b0f00d06de48195bdb4243249532209e144
Addison-Stavlo <AddieStavlo@gmail.com>
2019-02-10 19:24:33 -0500

    cta-btn in place, animates in

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const navBar = document.querySelector(".nav-bar");
 const slideAngle = document.querySelector(".slide-angle");
 const myName = document.querySelector(".my-name");
 const myTrade = document.querySelector(".my-trade");
+const ctaBtn = document.querySelector(".cta-btn");
 // console.log(triangle);
 // triangle.style.borderLeft = `${window.innerWidth * 0.7}px solid transparent`;
 // triangle.style.borderBottom = `${window.innerHeight}px solid #555`;
@@ -25,12 +26,16 @@ document.onreadystatechange = () => {
     y: -5000,
     ease: Sine.easeOut,
     onComplete: () => {
-      //   navBar.removeAttribute("style");
+      ctaBtn.style.display = `inline-block`;
+      TweenMax.from(ctaBtn, 1, { opacity: 0 });
     }
   });
   TweenMax.from(slideAngle, 0.6, { right: -1000, ease: Sine.easeOut });
   TweenMax.from(myName, 2, { opacity: 0 });
-  TweenMax.from(myTrade, 2, { opacity: 0 });
+  TweenMax.from(myTrade, 2, {
+    opacity: 0,
+    onComplete: () => {}
+  });
 };
 window.onload = () => {
   //   triangle.style.borderBottom = `${window.innerHeight}px solid #555`;
